04828dcd61f92be71afb3458e7e32169103d2383
robertcode200 <robertcode200@gmail.com>
2025-07-23 06:45:00 +0800

    feat: integrate service getLatestTradeDates

---
+++ src/states/marketInsight/marketInsightApiSlice.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type DateString = `${string & { __brand: "\\d{4}-\\d{2}-\\d{2}" }}`;
+type ResponseLatestTradeDates = { date: DateString }[];
+type TransformedLatestTradeDates = { dateString: DateString, unixTimestampSeconds: number }[];
+
+export const marketInsightApiSlice = createApi({
+  reducerPath: "marketInsight",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.market-insight.futures-ai.com",
+  }),
+  endpoints: (builder) => {
+    return {
+      // ## market_info ##
+      getLatestTradeDates: builder.query<TransformedLatestTradeDates, { size: number }>({
+        query: ({ size }) =>
+          `api/market_info/latest_trade_dates?size=${size}`,
+        transformResponse: (response: ResponseLatestTradeDates) => {
+          if (response.length === 0) return [];
+          return response.map(({ date }) => ({
+            dateString: date,
+            unixTimestampSeconds: Math.floor(new Date(date).getTime() / 1000),
+          }));
+        }
+      }),
+    };
+  },
+});
+
+export const { useGetLatestTradeDatesQuery } = marketInsightApiSlice;
--- src/states/store.ts
+++ src/states/store.ts
@@ -1,7 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { marketInsightApiSlice } from './marketInsight/marketInsightApiSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [marketInsightApiSlice.reducerPath]: marketInsightApiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(marketInsightApiSlice.middleware);
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
