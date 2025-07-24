473e1e345ffc5c95d44b620863a59edae22b2c7b
robertcode200 <robertcode200@gmail.com>
2025-07-24 10:39:45 +0800

    refactor: remove unnecessary test files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Test from '@/components/Test';
 import DailyChipsPage from '@/pages/DailyChipsPage';
 import './App.css';
 
 const App = () =>{
   return (
     <div>
-      <h1>App</h1>
-      <Test />
       <DailyChipsPage />
     </div>
   );
--- src/pages/DailyChipsPage/DailyChipsPage.tsx
+++ src/pages/DailyChipsPage/DailyChipsPage.tsx
@@ -1,14 +1,9 @@
-import ChipOverview from "./tabContents/chipOverview/ChipOverview";
-import ChipCarryOver from "./tabContents/chipCarryOver/ChipCarryOver";
 import NavGroup from "./NavGroup";
 
 const DailyChipsPage = () => {
   return (
     <div>
       <h1>Daily Chips Page</h1>
-      <p>This is the daily chips page component.</p>
-      <ChipOverview />
-      <ChipCarryOver />
       <NavGroup />
     </div>
   );
